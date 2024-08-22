9825d24a87964d12dc07be352510567a5d26d0f8
ewasince <ewasince@gmail.com>
2024-08-22 22:22:02 +0300

    change

--- main.go
+++ main.go
@@ -1,75 +1,171 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"time"
 
-	//"github.com/appleboy/gin-jwt/v2"
+	jwt "github.com/appleboy/gin-jwt/v2"
 	"github.com/gin-gonic/gin"
 )
 
-var db = make(map[string]string)
+type login struct {
+	Username string `form:"username" json:"username" binding:"required"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
 
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
+var (
+	identityKey = "id"
+	port        string
+)
 
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+// User demo
+type User struct {
+	UserName  string
+	FirstName string
+	LastName  string
+}
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
-
-	return r
+func init() {
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "8181"
+	}
 }
 
 func main() {
-	r := setupRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	engine := gin.Default()
+	// the jwt middleware
+	authMiddleware, err := jwt.New(initParams())
+	if err != nil {
+		log.Fatal("JWT Error:" + err.Error())
+	}
+
+	// register middleware
+	engine.Use(handlerMiddleWare(authMiddleware))
+
+	// register route for auth
+	registerRoute(engine, authMiddleware)
+
+	// start http server
+	if err = http.ListenAndServe(":"+port, engine); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func registerRoute(r *gin.Engine, handle *jwt.GinJWTMiddleware) {
+	r.POST("/login", handle.LoginHandler)
+	r.NoRoute(handle.MiddlewareFunc(), handleNoRoute())
+
+	auth := r.Group("/auth", handle.MiddlewareFunc())
+	auth.GET("/refresh_token", handle.RefreshHandler)
+	auth.GET("/hello", helloHandler)
+}
+
+func handlerMiddleWare(authMiddleware *jwt.GinJWTMiddleware) gin.HandlerFunc {
+	return func(context *gin.Context) {
+		errInit := authMiddleware.MiddlewareInit()
+		if errInit != nil {
+			log.Fatal("authMiddleware.MiddlewareInit() Error:" + errInit.Error())
+		}
+	}
+}
+
+func initParams() *jwt.GinJWTMiddleware {
+
+	return &jwt.GinJWTMiddleware{
+		Realm:       "test zone",
+		Key:         []byte("secret key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: identityKey,
+		PayloadFunc: payloadFunc(),
+
+		IdentityHandler: identityHandler(),
+		Authenticator:   authenticator(),
+		Authorizator:    authorizator(),
+		Unauthorized:    unauthorized(),
+		TokenLookup:     "header: Authorization, query: token, cookie: jwt",
+		// TokenLookup: "query:token",
+		// TokenLookup: "cookie:token",
+		TokenHeadName: "Bearer",
+		TimeFunc:      time.Now,
+	}
+}
+
+func payloadFunc() func(data interface{}) jwt.MapClaims {
+	return func(data interface{}) jwt.MapClaims {
+		if v, ok := data.(*User); ok {
+			return jwt.MapClaims{
+				identityKey: v.UserName,
+			}
+		}
+		return jwt.MapClaims{}
+	}
+}
+
+func identityHandler() func(c *gin.Context) interface{} {
+	return func(c *gin.Context) interface{} {
+		claims := jwt.ExtractClaims(c)
+		return &User{
+			UserName: claims[identityKey].(string),
+		}
+	}
+}
+
+func authenticator() func(c *gin.Context) (interface{}, error) {
+	return func(c *gin.Context) (interface{}, error) {
+		var loginVals login
+		if err := c.ShouldBind(&loginVals); err != nil {
+			return "", jwt.ErrMissingLoginValues
+		}
+		userID := loginVals.Username
+		password := loginVals.Password
+
+		if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
+			return &User{
+				UserName:  userID,
+				LastName:  "Bo-Yi",
+				FirstName: "Wu",
+			}, nil
+		}
+		return nil, jwt.ErrFailedAuthentication
+	}
+}
+
+func authorizator() func(data interface{}, c *gin.Context) bool {
+	return func(data interface{}, c *gin.Context) bool {
+		if v, ok := data.(*User); ok && v.UserName == "admin" {
+			return true
+		}
+		return false
+	}
+}
+
+func unauthorized() func(c *gin.Context, code int, message string) {
+	return func(c *gin.Context, code int, message string) {
+		c.JSON(code, gin.H{
+			"code":    code,
+			"message": message,
+		})
+	}
+}
+
+func handleNoRoute() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		claims := jwt.ExtractClaims(c)
+		log.Printf("NoRoute claims: %#v\n", claims)
+		c.JSON(404, gin.H{"code": "PAGE_NOT_FOUND", "message": "Page not found"})
+	}
+}
+
+func helloHandler(c *gin.Context) {
+	claims := jwt.ExtractClaims(c)
+	user, _ := c.Get(identityKey)
+	c.JSON(200, gin.H{
+		"userID":   claims[identityKey],
+		"userName": user.(*User).UserName,
+		"text":     "Hello World.",
+	})
 }
