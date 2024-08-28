35770deac4b9e7f319895ef909014de39015296a
ewasince <ewasince@gmail.com>
2024-08-28 20:05:20 +0300

    back: default code

--- errors/core.go
+++ errors/core.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var defaultErrorCode int16 = 400
+var defaultErrorCode int16 = 404
 
 type webError struct {
 	error
--- errors/errors.go
+++ errors/errors.go
@@ -1,5 +1,7 @@
 package errors
 
+var DEFAULT_CODE = "ERROR"
+
 // var UserNotAuthorized = errors.New("incorrect Username or Password")
 var UserNotFound = newError("user not found", "USER_NOT_FOUND", nil)
 var PatientNotFound = newError("patient not found", "PATIENT_NOT_FOUND", nil)
