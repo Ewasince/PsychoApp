fe00096cf959d8a4cf49f80d24e88739d3851681
ewasince <ewasince@gmail.com>
2024-09-01 14:58:55 +0300

    front: norm account

--- psycho-app-admin/src/Components/MainPage/Account/Account.tsx
+++ psycho-app-admin/src/Components/MainPage/Account/Account.tsx
@@ -1,9 +1,8 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {getMe, IMe} from "../../../api/endpoints/apiAuth";
 import {handleError} from "../../../core/errors";
-
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import {Heading} from "../../componetsCore";
 
 export const Account = () => {
     const [user, setUser] = useState<IMe>();
@@ -22,19 +21,23 @@ export const Account = () => {
     }, [])
 
     return (
-        <div className="bg-secondary-color rounded-lg sm:rounded-xl p-4 sm:p-6 xl:p-8">
-            <div className="flex flex-col md:flex-row gap-8 md:items-center">
-                <AccountBoxIcon/>
-                {/*<img src={AccountBoxIcon} alt="account icon" className="w-28"/>*/}
-                <div className="flex flex-col gap-2 sm:gap-4">
-                    <p className="text-lg sm:text-xl">Username: {user ? user.username : ''}</p>
-                    {/*<p className="text-lg sm:text-xl">Статус: {user ? user. : ''}</p>*/}
-                    {/*<p className="text-lg sm:text-xl">Создан: {user ? user.created_at.toString() : ''}</p>*/}
-                    {/*<p className="text-lg sm:text-xl">Телефон: {user ? user.created_at : ''}</p>*/}
-                    {/*<p className="text-lg sm:text-xl">Имя: {user ? user. : ''}</p>*/}
-                    {/*<p className="text-lg sm:text-xl">Фамилия: {user ? user.lastname : ''}</p>*/}
+
+        <div
+            className="w-full bg-secondary-color rounded-lg sm:rounded-xl p-4 sm:p-6 xl:p-8 text-font-color flex flex-col">
+            <Heading
+                heading="Профиль"
+            />
+
+            <div className="container mt-5">
+                <div className="entry-container flex flex-col-reverse 2xl:flex-row gap-1 sm:gap-2">
+                    <div className="flex flex-col w-full 2xl:max-w-[310px]">
+                        <p className="text-lg sm:text-xl flex flex space-x-2">
+                            <div className="font-medium">Имя: </div>
+                            <div className="mr-2" >{user?.username}</div>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-    )
+)
 }
