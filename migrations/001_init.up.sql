898466d450958ed77eb825d9169c27f30c8efae5
ewasince <ewasince@gmail.com>
2024-09-11 22:24:59 +0300

    back: rename tables

--- migrations/001_init.up.sql
+++ migrations/001_init.up.sql
@@ -1,20 +1,20 @@
-CREATE TABLE users
+CREATE TABLE user
 (
     id    INTEGER PRIMARY KEY AUTOINCREMENT,
     name  TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE patients
+CREATE TABLE patient
 (
     id      INTEGER PRIMARY KEY AUTOINCREMENT,
     name    TEXT NOT NULL,
     email   TEXT NOT NULL UNIQUE,
     user_id INTEGER  NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
-CREATE TABLE stories
+CREATE TABLE story
 (
     id         INTEGER PRIMARY KEY AUTOINCREMENT,
     date       DATE NOT NULL,
@@ -23,5 +23,5 @@ CREATE TABLE stories
     emotion    TEXT NOT NULL,
     power      INTEGER  NOT NULL,
     patient_id INTEGER  NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES patients (id)
+    FOREIGN KEY (patient_id) REFERENCES patient (id)
 );
