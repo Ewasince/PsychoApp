22c46e92783fd950a6f03221d6bb7b2111628bfa
ewasince <ewasince@gmail.com>
2024-09-12 20:52:03 +0300

    back: fix bug

--- backend/storageRepo/patients.go
+++ backend/storageRepo/patients.go
@@ -6,13 +6,12 @@ import (
 
 func GetPatients(userId uint) (*[]Patient, error) {
 	var patients []Patient
-	err := DB.Where("UserId = ?", userId).Find(&patients).Error
+	err := DB.Where("user_id = ?", userId).Find(&patients).Error
 
 	return &patients, err
 }
 
 func GetPatient(patientId uint) (*Patient, error) {
-	// STUB: !!!
 	var patient Patient
 	err := DB.Find(&patient, patientId).Error
 
