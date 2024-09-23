0ab63cb1b4e9e68d1752e473e0a8d64462aa44b1
ewasince <ewasince@gmail.com>
2024-09-23 22:08:52 +0300

    front: cleanup error handler

--- psycho-app-admin/src/core/errors.tsx
+++ psycho-app-admin/src/core/errors.tsx
@@ -1,6 +1,5 @@
 // import {refreshToken} from "../api/apiToken";
 import {NavigateFunction} from "react-router-dom";
-import {refreshToken} from "../api/apiRefreshToken";
 import {getRefreshToken} from "./storage/tokens";
 import {isUserEntered} from "../api/userControl";
 import {toast} from "react-toastify";
@@ -29,16 +28,6 @@ export function handleError(error: any, navigate?: NavigateFunction) {
             if (!getRefreshToken()) {
                 goToAuthUser(navigate)
             }
-
-            refreshToken((err) => {
-                if (err === 200) {
-                    window.location.reload();
-                }
-                logErrorToConsole()
-                if (err === 401) {
-                    goToAuthUser(navigate)
-                }
-            })
             return
         }
         if (error.response.status !== 403) {
@@ -63,50 +52,3 @@ export function goToAuthUser(navigate: NavigateFunction) {
 function errorToast(error: any) {
     toast.error("Невозможно выполнить запрос: " + error.message)
 }
-
-
-// export function handleError(error: any, navigate?: any, refresh = true) {
-//
-//     function logErrorInner() {
-//         console.log(error.response.data);
-//         console.log(error.response.status);
-//         console.log(error.response.headers);
-//     }
-//
-//     // return
-//     if (error.response) {
-//
-//         if (!navigate) {
-//             logErrorInner()
-//             return
-//         }
-//         if (error.response.status == 401) {
-//             navigate('/')
-//             logErrorInner()
-//             return
-//         }
-//         if (error.response.status != 403) {
-//             logErrorInner()
-//             return
-//         }
-//         if (!refresh) {
-//             logErrorInner()
-//             return
-//         }
-//
-//         refreshToken((err) => {
-//             if (err === 401) {
-//                 console.log("handleError refreshToken err === 401")
-//                 logErrorInner()
-//                 navigate('/')
-//                 return
-//             }
-//             window.location.reload();
-//         })
-//     } else if (error.request) {
-//         console.log(error.request);
-//     } else {
-//         console.log('Error', error.message);
-//     }
-//     // console.log(error.config);
-// }
