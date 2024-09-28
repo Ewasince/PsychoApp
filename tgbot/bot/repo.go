909c93566397689472c961ce4ee1df123bdd8198
ewasince <ewasince@gmail.com>
2024-09-28 13:16:32 +0300

    oth!

--- tgbot/bot/repo.go
+++ tgbot/bot/repo.go
@@ -6,10 +6,10 @@ import (
 	"time"
 )
 
-func IsPatientRegistered(tgId int64) bool {
-	_, patientErr := repo.GetPatientByTg(tgId)
-	return patientErr == nil
-}
+//func IsPatientRegistered(tgId int64) bool {
+//	_, patientErr := repo.GetPatientByTg(tgId)
+//	return patientErr == nil
+//}
 
 func LoadStory(story *models.Story) error {
 	story.Date = getDate()
