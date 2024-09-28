6b1167ee6dd7351dd6c9e7e4ad4a310179b23fa0
ewasince <ewasince@gmail.com>
2024-09-28 13:17:02 +0300

    bot: fix cache

--- tgbot/cache/storyCache.go
+++ tgbot/cache/storyCache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	. "StorageModule/models"
+	"gorm.io/gorm"
 )
 
 type StoriesMap map[int64]*Story
@@ -11,12 +12,16 @@ var storiesCache = map[int64]*Story{}
 func GetStory(patientId int64) *Story {
 	story, exists := storiesCache[patientId]
 	if !exists {
-		return ResetStory(patientId)
+		return nil
 	}
 	return story
 }
 
 func ResetStory(patientId int64) *Story {
-	storiesCache[patientId] = &Story{}
+	storiesCache[patientId] = &Story{
+		BaseModel: BaseModel{
+			Model: gorm.Model{},
+		},
+	}
 	return storiesCache[patientId]
 }
