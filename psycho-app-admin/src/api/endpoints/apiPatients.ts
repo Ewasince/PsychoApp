974ab292df27931e47dafd38568988d67b7b4c44
ewasince <ewasince@gmail.com>
2024-10-05 19:03:16 +0300

    es!

--- psycho-app-admin/src/api/endpoints/apiPatients.ts
+++ psycho-app-admin/src/api/endpoints/apiPatients.ts
@@ -20,6 +20,12 @@ export type IStory = {
     emotionPower: number
 }
 
+export type IMood = {
+    id: number
+    date: number
+    value: number
+}
+
 export const getPatients = async () => {
     return await credentialsRequest.get<IPatient[]>(
         PATIENTS_URL,
@@ -32,38 +38,42 @@ export const getPatient = async (patientId: string) => {
     )
 }
 
-export type IStoriesMinDate = {
+export type IDataRangeMinDate = {
     minDate: number
 }
 export type IStoriesResponse = {
     stories: IStory[]
 }
-export type IStoriesParams = {
+export type IDataRangeParams = {
     dateStart: number
     dateFinish: number
 }
 
 export const getPatientStoriesMinDate = async (patientId: string) => {
-    return await credentialsRequest.get<IStoriesMinDate>(
+    return await credentialsRequest.get<IDataRangeMinDate>(
         generateUrl(PATIENTS_URL, patientId, "story"),
     )
 }
 
-export const getPatientStories = async (patientId: string, params?: IStoriesParams) => {
+export const getPatientStories = async (patientId: string, params?: IDataRangeParams) => {
     return await credentialsRequest.get<IStoriesResponse>(
         generateUrl(PATIENTS_URL, patientId, "story"),
         params ? {params: params} : {},
     )
 }
 
+export type IMoodsResponse = {
+    moods: IMood[]
+}
+
 export const getPatientMoodsMinDate = async (patientId: string) => {
-    return await credentialsRequest.get<IStoriesMinDate>(
+    return await credentialsRequest.get<IDataRangeMinDate>(
         generateUrl(PATIENTS_URL, patientId, "mood"),
     )
 }
 
-export const getPatientMoods = async (patientId: string, params?: IStoriesParams) => {
-    return await credentialsRequest.get<IStoriesResponse>(
+export const getPatientMoods = async (patientId: string, params?: IDataRangeParams) => {
+    return await credentialsRequest.get<IMoodsResponse>(
         generateUrl(PATIENTS_URL, patientId, "mood"),
         params ? {params: params} : {},
     )
