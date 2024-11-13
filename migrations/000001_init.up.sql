500ff7731c9d6ac89dcc41190f91560fae5f82c2
Ewasince <ewasince@gmail.com>
2024-11-13 14:54:03 +0300

    mig: pretty sql mig

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users
 (
     id         SERIAL PRIMARY KEY,
-    name       TEXT NOT NULL,
-    email      TEXT NOT NULL UNIQUE,
-    username   TEXT NOT NULL UNIQUE,
-    password   TEXT NOT NULL,
+    name       TEXT                     NOT NULL,
+    email      TEXT                     NOT NULL UNIQUE,
+    username   TEXT                     NOT NULL UNIQUE,
+    password   TEXT                     NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE,
     deleted_at TIMESTAMP WITH TIME ZONE,
@@ -14,14 +14,14 @@ CREATE TABLE users
 CREATE TABLE patients
 (
     id            SERIAL PRIMARY KEY,
-    name          TEXT    NOT NULL,
+    name          TEXT                     NOT NULL,
     last_name     TEXT,
     email         TEXT,
-    username      TEXT    NOT NULL,
-    password      TEXT    NOT NULL,
-    user_id       INTEGER NOT NULL REFERENCES users (id),
+    username      TEXT                     NOT NULL,
+    password      TEXT                     NOT NULL,
+    user_id       INTEGER                  NOT NULL REFERENCES users (id),
     tg_id         INTEGER,
-    created_at    TIMESTAMP WITH TIME ZONE    NOT NULL,
+    created_at    TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at    TIMESTAMP WITH TIME ZONE,
     deleted_at    TIMESTAMP WITH TIME ZONE,
     next_schedule TIMESTAMP WITH TIME ZONE,
@@ -32,13 +32,13 @@ CREATE TABLE patients
 CREATE TABLE stories
 (
     id         SERIAL PRIMARY KEY,
-    date       TIMESTAMP WITH TIME ZONE    NOT NULL,
-    situation  TEXT    NOT NULL,
-    mind       TEXT    NOT NULL,
-    emotion    TEXT    NOT NULL,
-    power      INTEGER NOT NULL,
-    patient_id INTEGER NOT NULL REFERENCES patients (id),
-    created_at TIMESTAMP WITH TIME ZONE    NOT NULL,
+    date       TIMESTAMP WITH TIME ZONE NOT NULL,
+    situation  TEXT                     NOT NULL,
+    mind       TEXT                     NOT NULL,
+    emotion    TEXT                     NOT NULL,
+    power      INTEGER                  NOT NULL,
+    patient_id INTEGER                  NOT NULL REFERENCES patients (id),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE,
     deleted_at TIMESTAMP WITH TIME ZONE,
     attention  INTEGER,
@@ -48,10 +48,10 @@ CREATE TABLE stories
 CREATE TABLE moods
 (
     id         SERIAL PRIMARY KEY,
-    patient_id INTEGER NOT NULL REFERENCES patients (id),
-    date       TIMESTAMP WITH TIME ZONE    NOT NULL,
-    value      INTEGER NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE    NOT NULL,
+    patient_id INTEGER                  NOT NULL REFERENCES patients (id),
+    date       TIMESTAMP WITH TIME ZONE NOT NULL,
+    value      INTEGER                  NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE,
     deleted_at TIMESTAMP WITH TIME ZONE
 );
@@ -59,7 +59,7 @@ CREATE TABLE moods
 CREATE TABLE invites
 (
     id         SERIAL PRIMARY KEY,
-    email      TEXT NOT NULL,
+    email      TEXT                     NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE,
     deleted_at TIMESTAMP WITH TIME ZONE
